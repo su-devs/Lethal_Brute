2b207a7fe32a750a9084e71a68492be6b5cb3d30
Just Gin <70573088+g1ng3rb1t3@users.noreply.github.com>
2020-09-11 11:46:58 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+echo -ne "Press enter to install dependencies> "
+read enter
+pkg install toilet
+clear
+echo -e "[+] toilet installed"
+sleep 1
+pkg install figlet
+clear
+echo -e "[+] Figlet installed"
+sleep 1
+pkg install python
+clear
+echo -e "[+] Python installed"
+sleep 1
+clear
+sleep 3
+echo -e "All Dependencies installed....."
+sleep 3
+clear
+echo -e "Now use python3 lebrute.py [-option]"
+echo -e "Created by (g1ng3rb1t3) kevo"
+sleep 2
+exit
